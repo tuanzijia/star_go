7f3d469c5ca5018792ecdd826a64524a366909f8
团子家 <706561551@qq.com>
2019-10-31 17:33:14 +0800

    新增读取json文件方法

--- util_file.go
+++ util_file.go
@@ -1,6 +1,10 @@
 package starGo
 
-import "os"
+import (
+	"encoding/json"
+	"io/ioutil"
+	"os"
+)
 
 // 文件夹是否存在(obsolete)
 func IsDirExists(path string) bool {
@@ -24,4 +28,18 @@ func IsFileExists(path string) (bool, error) {
 	}
 
 	return true, err
+}
+
+// 读取json文件
+func ReadJsonFile(path string, v interface{}) error {
+	data, err := ioutil.ReadFile(path)
+	if err != nil {
+		return err
+	}
+
+	err = json.Unmarshal(data, v)
+	if err != nil {
+		return err
+	}
+	return nil
 }
