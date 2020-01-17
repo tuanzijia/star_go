b322ff8d91abd2fb6866ba217a3ca3fedc3ca793
刘星源 <706561551@qq.com>
2020-01-17 21:16:44 +0800

    调整随机数的命名

--- util_rand.go
+++ util_rand.go
@@ -5,7 +5,7 @@ import (
 	"math"
 )
 
-func Int64() int64 {
+func RandInt64() int64 {
 	result, err := rand.Int(rand.Reader, maxBigInt64Edge)
 	if err != nil {
 		return math.MaxInt64
@@ -14,62 +14,62 @@ func Int64() int64 {
 	return result.Int64()
 }
 
-func Int64n(n int64) int64 {
+func RandInt64n(n int64) int64 {
 	if n <= 0 {
 		return 0
 	}
 	if n&(n-1) == 0 {
-		return Int64() & (n - 1)
+		return RandInt64() & (n - 1)
 	}
 	max := int64((1 << 63) - 1 - (1<<63)%uint64(n))
-	v := Int64()
+	v := RandInt64()
 	for v > max {
-		v = Int64()
+		v = RandInt64()
 	}
 
 	return v % n
 }
 
-func Uint64() uint64 {
-	return uint64(Int64())>>31 | uint64(Int64())<<32
+func RandUint64() uint64 {
+	return uint64(RandInt64())>>31 | uint64(RandInt64())<<32
 }
 
-func Uint32() uint32 {
-	return uint32(Int64() >> 31)
+func RandUint32() uint32 {
+	return uint32(RandInt64() >> 31)
 }
 
-func Int32() int32 {
-	return int32(Int64() >> 32)
+func RandInt32() int32 {
+	return int32(RandInt64() >> 32)
 }
 
-func Int32n(n int32) int32 {
+func RandInt32n(n int32) int32 {
 	if n <= 0 {
 		return 0
 	}
 	if n&(n-1) == 0 {
-		return Int32() & (n - 1)
+		return RandInt32() & (n - 1)
 	}
 	max := int32((1 << 31) - 1 - (1<<31)%uint32(n))
-	v := Int32()
+	v := RandInt32()
 	for v > max {
-		v = Int32()
+		v = RandInt32()
 	}
 	return v % n
 }
 
-func Int() int {
-	u := uint(Int64())
+func RandInt() int {
+	u := uint(RandInt64())
 	return int(u << 1 >> 1)
 }
 
-func IntN(n int) int {
+func RandIntN(n int) int {
 	if n <= 0 {
 		return 0
 	}
 	if n <= 1<<31-1 {
-		return int(Int32n(int32(n)))
+		return int(RandInt32n(int32(n)))
 	}
-	return int(Int64n(int64(n)))
+	return int(RandInt64n(int64(n)))
 }
 
 func Shuffle(n int, swap func(i, j int)) {
@@ -78,11 +78,11 @@ func Shuffle(n int, swap func(i, j int)) {
 	}
 	i := n - 1
 	for ; i > 1<<31-1-1; i-- {
-		j := int(Int64n(int64(i + 1)))
+		j := int(RandInt64n(int64(i + 1)))
 		swap(i, j)
 	}
 	for ; i > 0; i-- {
-		j := int(Int32n(int32(i + 1)))
+		j := int(RandInt32n(int32(i + 1)))
 		swap(i, j)
 	}
 }
